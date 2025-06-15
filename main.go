89ebd7d1d744cb7ac31cb47c6aa8e5fbebaa34dc
Nachiket <nachiket.upadhye@stud.hs-emden-leer.de>
2025-06-15 13:40:25 +0200

    how to scan a values

--- main.go
+++ main.go
@@ -10,11 +10,14 @@ func main() {
 	var investment float64 = 1000
 	years := 10
 	returns := 5.5
+	fmt.Println("Investment Calculator Enter your amount here: ")
+	// scanning the function for the input
+	fmt.Scan(&investment)
+	// This is a pointer to the investment variable
 	futureValues := investment * math.Pow(1+returns/100, float64(years))
 	fmt.Printf("Future value of the investment: %.2f\n", futureValues)
 
 	futureValuesadjusted := futureValues / math.Pow(1+inflationRate/100, float64(years))
 	fmt.Printf("Future value adjusted for inflation: %.2f\n", futureValuesadjusted)
-	fmt.Println("Hello Deshpandey, welcome to Go programming!")
 
 }
