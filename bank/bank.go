06df496bc960d5b1c62ac8aad80712e7c6c3ed11
Nachiket <nachuzone@gmail.com>
2025-06-15 16:12:11 +0200

    simple if else for bank application

--- bank/bank.go
+++ bank/bank.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+
+	var balance float64 = 1000.00 // initial balance
 	fmt.Println("Welcome to Bank Application")
 	fmt.Println("Please select an option from the menu below:")
 
@@ -16,5 +18,25 @@ func main() {
 	fmt.Print("Enter your choice: ") // taking input from the user
 	fmt.Scanln(&choice)
 	fmt.Println("You selected option:", choice)
+
+	if choice == 1 {
+		fmt.Println("Checking balance...", balance)
+	} else if choice == 2 {
+		fmt.Print("Enter the amount to deposit: ")
+		var depositeAmount float64
+		fmt.Scanln(&depositeAmount)
+		balance = balance + depositeAmount
+		fmt.Println("New balance after deposit is:", balance)
+	} else if choice == 3 {
+		fmt.Print("Enter the amount to withdraw: ")
+		var withdrawAmount float64
+		fmt.Scanln(&withdrawAmount)
+		if withdrawAmount > balance {
+			fmt.Println("Insufficient balance!")
+		} else {
+			balance = balance - withdrawAmount
+			fmt.Println("New balance after withdrawal is:", balance)
+		}
+	}
 
 }
